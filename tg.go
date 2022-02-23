b7af7bbf6a88f362cf901ff5d5fed7bad35ca827
Alex Kataev <dlyavsehpisem@gmail.com>
2022-02-23 13:27:37 +0500

    refactoring after linters

--- tg.go
+++ tg.go
@@ -11,85 +11,86 @@ import (
 	"time"
 )
 
-var tgAPIServer = "https://api.telegram.org"
+var apiServer = "https://api.telegram.org" //nolint
 
-type TGUser struct {
+type User struct {
 	ID        int64  `json:"id"`
-	FirstName string `json:"first_name"`
+	FirstName string `json:"first_name"` //nolint
 	UserName  string `json:"username,omitempty"`
 }
 
-type TGChat struct {
-	ChatID    int64  `json:"chat_id,omitempty"`
+type Chat struct {
+	ChatID    int64  `json:"chat_id,omitempty"` //nolint
 	Text      string `json:"text,omitempty"`
-	ParseMode string `json:"parse_mode,omitempty"`
+	ParseMode string `json:"parse_mode,omitempty"` //nolint
 }
 
-type TGMessage struct {
-	MessageID int `json:"message_id"`
+type Message struct {
+	MessageID int `json:"message_id"` //nolint
 	Date      int `json:"date"`
 }
 
-type TGAPIResponse struct {
+type APIResponse struct {
 	Result interface{} `json:"result,omitempty"`
-	TGAPIResponseError
+	APIResponseError
 }
 
-type TGAPIResponseError struct {
+type APIResponseError struct {
 	Ok          bool   `json:"ok"`
-	ErrorCode   int    `json:"error_code,omitempty"`
+	ErrorCode   int    `json:"error_code,omitempty"` //nolint
 	Description string `json:"description,omitempty"`
 	Parameters  struct {
-		RetryAfter int `json:"retry_after,omitempty"`
+		RetryAfter int `json:"retry_after,omitempty"` //nolint
 	} `json:"parameters,omitempty"`
 }
 
-func (r TGAPIResponseError) Error() string {
+func (r APIResponseError) Error() string {
 	return r.Description
 }
 
-type tgAPIMethod string
+type apiMethod string
 
 const (
-	methodGetMe       tgAPIMethod = "getMe"
-	methodSendMessage tgAPIMethod = "sendMessage"
+	apiMethodGetMe       apiMethod = "getMe"
+	apiMethodSendMessage apiMethod = "sendMessage"
 )
 
-type TG interface {
-	GetMe(ctx context.Context) (*TGUser, error)
-	SendMessage(ctx context.Context, chatID int64, message string) (*TGMessage, error)
+type tg interface {
+	GetMe(ctx context.Context) (*User, error)
+	SendMessage(ctx context.Context, chatID int64, text string) (*Message, error)
 }
 
 type httpClient interface {
 	Do(*http.Request) (*http.Response, error)
 }
 
-func NewTG(token string) TG {
-	client := &http.Client{
-		Timeout: 2 * time.Second,
-		Transport: &http.Transport{
-			MaxIdleConns:    10,
-			IdleConnTimeout: 10 * time.Second,
-		},
+var _ tg = (*TG)(nil)
+
+func NewTG(token string) *TG {
+	client := http.DefaultClient
+	client.Timeout = 2 * time.Second    //nolint
+	client.Transport = &http.Transport{ //nolint
+		MaxIdleConns:    10,               //nolint
+		IdleConnTimeout: 10 * time.Second, //nolint
 	}
 
 	return NewTGWithClient(token, client)
 }
 
-func NewTGWithClient(token string, client *http.Client) TG {
-	return &tg{
+func NewTGWithClient(token string, client *http.Client) *TG {
+	return &TG{
 		http:     client,
-		endpoint: fmt.Sprintf("%s/bot%s/", tgAPIServer, token),
+		endpoint: fmt.Sprintf("%s/bot%s/", apiServer, token),
 	}
 }
 
-type tg struct {
+type TG struct {
 	http     httpClient
 	endpoint string
 }
 
-func (_ *tg) makeMessage(chatID int64, text string) (io.Reader, error) {
-	chat := &TGChat{
+func (t *TG) makeMessage(chatID int64, text string) (io.Reader, error) {
+	chat := &Chat{
 		ChatID:    chatID,
 		Text:      text,
 		ParseMode: "markdown",
@@ -97,17 +98,18 @@ func (_ *tg) makeMessage(chatID int64, text string) (io.Reader, error) {
 
 	body, err := json.Marshal(chat)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("makeMessage: json: %w", err)
 	}
 
 	return bytes.NewReader(body), nil
 }
 
-func (t *tg) makeRequest(ctx context.Context, method tgAPIMethod, reader io.Reader) (*http.Request, error) {
+func (t *TG) makeRequest(ctx context.Context, method apiMethod, reader io.Reader) (*http.Request, error) {
 	url := t.endpoint + string(method)
+
 	req, err := http.NewRequestWithContext(ctx, "POST", url, reader)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("makeMessage: request: %w", err)
 	}
 
 	req.Header.Add("Content-Type", "application/json")
@@ -115,69 +117,85 @@ func (t *tg) makeRequest(ctx context.Context, method tgAPIMethod, reader io.Read
 	return req, nil
 }
 
-func (_ *tg) makeResponse(resp *http.Response, result interface{}) error {
+func (t *TG) makeResponse(resp *http.Response, result interface{}) error {
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		return fmt.Errorf("makeResponse: body: %w", err)
 	}
+
 	resp.Body.Close()
 
-	apiResp := &TGAPIResponse{
+	apiResp := &APIResponse{
 		Result: result,
+		APIResponseError: APIResponseError{
+			Ok:          false,
+			ErrorCode:   0,
+			Description: "",
+			Parameters: struct {
+				RetryAfter int "json:\"retry_after,omitempty\""
+			}{},
+		},
 	}
 
 	if err := json.Unmarshal(body, apiResp); err != nil {
-		return err
+		return fmt.Errorf("makeResponse: json: %w", err)
 	}
 
 	if !apiResp.Ok {
-		return apiResp.TGAPIResponseError
+		return apiResp.APIResponseError
 	}
 
 	return nil
 }
 
-func (t *tg) GetMe(ctx context.Context) (*TGUser, error) {
-	req, err := t.makeRequest(ctx, methodGetMe, nil)
+func (t *TG) GetMe(ctx context.Context) (*User, error) {
+	req, err := t.makeRequest(ctx, apiMethodGetMe, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	resp, err := t.http.Do(req)
 	if err != nil {
+		return nil, fmt.Errorf("GetMe: http: %w", err)
+	}
+
+	user := &User{
+		ID:        0,
+		FirstName: "",
+		UserName:  "",
+	}
+
+	if err := t.makeResponse(resp, user); err != nil {
 		return nil, err
 	}
 
-	tgUser := &TGUser{}
-
-	if err := t.makeResponse(resp, tgUser); err != nil {
-		return nil, err
-	}
-
-	return tgUser, nil
+	return user, nil
 }
 
-func (t *tg) SendMessage(ctx context.Context, chatID int64, message string) (*TGMessage, error) {
-	msg, err := t.makeMessage(chatID, message)
+func (t *TG) SendMessage(ctx context.Context, chatID int64, text string) (*Message, error) {
+	reader, err := t.makeMessage(chatID, text)
 	if err != nil {
 		return nil, err
 	}
 
-	req, err := t.makeRequest(ctx, methodSendMessage, msg)
+	req, err := t.makeRequest(ctx, apiMethodSendMessage, reader)
 	if err != nil {
 		return nil, err
 	}
 
 	resp, err := t.http.Do(req)
 	if err != nil {
+		return nil, fmt.Errorf("SendMessage: http: %w", err)
+	}
+
+	message := &Message{
+		MessageID: 0,
+		Date:      0,
+	}
+
+	if err := t.makeResponse(resp, message); err != nil {
 		return nil, err
 	}
 
-	tgMessage := &TGMessage{}
-
-	if err := t.makeResponse(resp, tgMessage); err != nil {
-		return nil, err
-	}
-
-	return tgMessage, nil
+	return message, nil
 }
